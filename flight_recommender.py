9592828c22f60bbf0910e199d0d1219b286f7610
omarTronto <44756630+omarTronto@users.noreply.github.com>
2019-05-20 17:02:29 +0800

    ShortestPath Task v1
    flight_recommender.py is updated
    
    I worked on representing the distances between cities as weighted undirected graphs, MapGraph class represents a complete graph, to construct the graph, create a MapGraph object, and then add roads between the cities by adding the distance between each 2 citiese, this can happen by retrieving these info from a table or a list prepared by the previous task by Ivan.
    
    * To get all the possible paths between 2 cities with the restriction of only 2-3 cities transit, call:
    all_possible_paths(source, destination)
    which returns a list of paths.
    
    * To print the graph, call
    print_graph()
    which prints the adjacency matrix of the graph.
    
    * To get the shortest path between 2 cities, call
    shortest_path(source, destination)
    which returns a list of (shortest path index, shortest path, distance of shortest path)

--- flight_recommender.py
+++ flight_recommender.py
@@ -14,8 +14,10 @@
 # 10. Lastly, calculate the total probability distribution of random routes taken for the end user to travel
 # from Kuala Lumpur to other country.
 
-import geopy as geo
 
+
+"""import geopy as geo"""
+import random
 
 class getBestFlight:
 	def __init__(self, city_a, city_b):
@@ -38,21 +40,153 @@ class getBestFlight:
 		return
 
 
+class City:
+	"""City objects are used as vertices in the MapGraph class."""
+	
+	def __init__(self, n):
+		self.name = n
+
+ 
+class MapGraph:
+	"""MapGraph is a class for constructing weighted undirected graphs using cities as vertices."""
+
+	def __init__ (self):
+		self.cities = {}
+		self.edges  = []
+		self.adj_list = {}
+		self.cities_indices = {}
+	
+
+	def add_road(self, city_x, city_y, distance):
+		"""To construct a new graph add all roads between cities"""
+
+		city_x = city_x.lower().strip()
+		city_y = city_y.lower().strip()
+
+		if city_x == city_y:
+			return
+		if city_x not in self.cities:
+			new_city_x = City (city_x)
+			self.cities[new_city_x.name] = new_city_x
+			self.adj_list[new_city_x.name] = []
+			for row in self.edges:
+				row.append(0)
+			self.edges.append([0] * (len(self.edges)+1))
+			self.cities_indices[new_city_x.name] = len(self.cities_indices)
+
+		if  city_y not in self.cities:
+			new_city_y = City (city_y)
+			self.cities[new_city_y.name] = new_city_y
+			self.adj_list[new_city_y.name] = []
+			for row in self.edges:
+				row.append(0)
+			self.edges.append([0] * (len(self.edges)+1))
+			self.cities_indices[new_city_y.name] = len(self.cities_indices)
+
+		if city_y not in self.adj_list[city_x]:
+			self.adj_list[city_x].append(city_y)
+		if city_x not in self.adj_list[city_y]:
+			self.adj_list[city_y].append(city_x)
+		
+		self.edges[self.cities_indices[city_x]][self.cities_indices[city_y]] = distance
+		self.edges[self.cities_indices[city_y]][self.cities_indices[city_x]] = distance
 
 
 
+	def find_all_paths (self, city_x, city_y, path = []):
+		"""This method is used as a backend method to support other methods in the class"""
+
+		city_x = city_x.lower().strip()
+		city_y = city_y.lower().strip()
+		
+		path = path + [city_x]
+		
+		if city_x not in self.cities:
+			return []
+		
+		if city_x == city_y:
+			return [path]
+		
+		paths = []
+		newpaths = []
+		
+		for city in self.adj_list[city_x]:
+			if city not in path:
+				newpaths = self.find_all_paths(city, city_y, path)
+			for newpath in newpaths:
+				if ( len(newpath)<4 or len(newpath)>5):
+					pass
+				else:
+					paths.append(newpath)
+		return paths
 
 
+	def all_possible_paths (self, city_x, city_y):
+		"""
+		To get all possible paths between 2 cities in the driver code.
+		It restricts the possible paths to be transitting through 2-3 cities only
+		"""
+
+		all_possible_paths = self.find_all_paths(city_x, city_y)
+		all_possible_paths = [list(t) for t in set(tuple(element) for element in all_possible_paths)]
+		return all_possible_paths
+
+ 
+	def shortest_path (self, city_x, city_y):
+		"""This method returns a list of (shortest path index, shortest path, distance of shortest path)"""
+
+		all_paths = self.all_possible_paths(city_x, city_y)
+		shortets_value = 10**9
+		shortest_path = []
+		count = 0
+		x = 0
+		for each_path in all_paths:
+			path_distance = 0
+			count += 1
+			for i in range(0,len(each_path)-1):
+				path_distance += self.edges[self.cities_indices[each_path[i]]][self.cities_indices[each_path[i+1]]]
+			if path_distance < shortets_value:
+				shortets_value = path_distance
+				shortest_path = each_path
+				x = count
+		if shortets_value == 10**9:
+			shortets_value = 0
+		return [x, shortest_path, shortets_value]
 
 
+	def print_graph(self):
+		"""This method prints the adjacency matrix"""
+
+		print("\t\t\t",end="")
+		for city, i in self.cities_indices.items():
+			print(city + '\t', end='')
+		print()
+		for city, i in self.cities_indices.items():
+			print(city + '\t\t', end='')
+			for j in range(len(self.edges)):
+				print(self.edges[i][j], end='\t\t')
+			print(' ')
 
 
+"""
+DRIVER CODE
+This is a randomized grapgh between 10 different states 
+and then finding shortest path between india and paskitan
+"""
 
-
-
-
-
-
+g = MapGraph()
+list1 = ["Japan","India","Malaysia","UAE","Turkey","China","SKorea","Taiwan","Singapore","Pakistan"]
+for city1 in list1:
+	for city2 in list1:
+		g.add_road(city1,city2,random.randint(100,1000))
+g.print_graph()
+print()
+paths = g.all_possible_paths("India","Pakistan")
+for path in paths:
+	print(path)
+shortest = g.shortest_path("India","Pakistan")
+print("\n")
+print(shortest)
 
 
 
