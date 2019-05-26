5d52cb92c8f8fffc85c026044fdffd962f1020d9
ivanlowe <50705928+ivanlowe@users.noreply.github.com>
2019-05-26 21:41:55 +0800

    Update and rename flight_recommender.py to shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -1,44 +1,4 @@
-# 1. Get and mark locations of 5 - 10 major cities in the world (where you can easily gets their main English
-# newspaper online) from Kuala Lumpur.
-# 2. Get the distances between these cities
-# 3. Using one of the algorithms for shortest path, get the minimum distance to travel to the destination from
-# Kuala Lumpur by transiting 2-3 cities.
-# 4. Plot line between the cities before and after the algorithm chosen.
-# 5. Extract from the English newspaper of the cities of webpages text and count the number of words in the
-# webpages.
-# 6. Plot line/scatter/histogram graphs related to the word count using Plotly (Word count, stop words)
-# 7. Compare words in the webpages with the positive, negative and neutral English words using a String
-# Matching algorithm
-# 8. Plot histogram graphs of positive and negative words found in the webpages.
-# 9. Give an algorithmic conclusion regarding the sentiment of those articles
-# 10. Lastly, calculate the total probability distribution of random routes taken for the end user to travel
-# from Kuala Lumpur to other country.
-
-
-
-"""import geopy as geo"""
 import random
-
-class getBestFlight:
-	def __init__(self, city_a, city_b):
-		self.a = city_a
-		self.b = city_b
-
-	def getDistance(self):
-		return
-
-	def markLocation(self):
-		return
-
-	def getShortestPath(self):
-		return
-
-	def plotCityLine(self):
-		return
-
-	def getTotalWordCount(self):
-		return
-
 
 class City:
 	"""City objects are used as vertices in the MapGraph class."""
