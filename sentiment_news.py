e297257e8f178dbac712afdc2b3aa48cf0891a2c
Hexareus <dashhax@gmail.com>
2019-05-26 13:49:54 +0800

    News sentiment analysis

--- sentiment_news.py
+++ sentiment_news.py
@@ -2,23 +2,30 @@ import json
 import requests
 from cache import Cache
 from newspaper import Article
+import sentiment_plotter
 
 class NewsSentiment:
     __positiveList = []
     __negativeList = []
+    __stopwords = []
 
     def __init__(self):
         self.__cache = Cache("news-sentiment.json")
 
-        pFile = open("positive.txt")
-        self.__positiveList = pFile.read().lower().split()
-        pFile.close()
+        with open("positive.txt") as pFile:
+            self.__positiveList = pFile.read().lower().split()
 
-        nFile = open("negative.txt")
-        self.__negativeList = nFile.read().lower().split()
-        nFile.close()
+        with open("negative.txt") as nFile:
+            self.__negativeList = nFile.read().lower().split()
+
+        with open("stopwords.txt") as sFile:
+            self.__stopwords = sFile.read().lower().split()
 
         self.__key = "4e28e4b30b954544b5d808b4d54b37a4";
+
+        self.__positiveCount = 0
+        self.__negativeCount = 0
+        self.__stopwordCount = 0
 
         with open("news-id.json") as nifile:
             self.__news_id = json.load(nifile)
@@ -52,15 +59,21 @@ class NewsSentiment:
     def calculate_polarity(self, word):
         polarity = 0
         parts = word.split()
-
+        p = 0
+        n = 0
+        s = 0
         for part in parts:
             part = part.strip().lower()
             if part in self.__positiveList:
                 polarity += 1
+                p += 1
             elif part in self.__negativeList:
                 polarity -= 1
+                n += 1
+            elif part in self.__stopwords:
+                s += 1
 
-        return polarity
+        return polarity, p, n, s
 
     def fetch_news_sentiment(self, country):
         api_id = ','.join(self.__news_id)
@@ -91,18 +104,26 @@ class NewsSentiment:
                 news.download()
                 news.parse()
 
-                title_score = self.calculate_polarity(title)
-                news_score = self.calculate_polarity(news.text)
+                title_score, tp, tn, ts = self.calculate_polarity(title)
+                news_score, np, nn, ns = self.calculate_polarity(news.text)
 
-                sentiment = {"title":title_score, "news":news_score, "total":(title_score + news_score)}
+                sentiment = {"title":title_score, "news":news_score, "total":(title_score + news_score), "stats":{"p":tp + np, "n": np + nn, "s": ts + ns}}
                 self.__cache.set(key, sentiment)
 
             sentiment = self.__cache.get(key)
+            self.__positiveCount += sentiment["stats"]["p"]
+            self.__negativeCount += sentiment["stats"]["n"]
+            self.__stopwordCount += sentiment["stats"]["s"]
+
             sentiments.append(sentiment)
 
         return sentiments
+
+    def showWordStatistics(self):
+        sentiment_plotter.plotResults(self.__positiveCount, self.__negativeCount, self.__stopwordCount)
 
 
 ns = NewsSentiment()
 p = ns.fetch_news_sentiment("Jakarta")
 print(p)
+ns.showWordStatistics()
---
+++ sentiment_plotter.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+
+def plotResults(p, n, s):
+    objects = ("Positive Words", "Negative Words", "Stop words")
+    y_pos = np.arange(len(objects))
+    values = [p, n, s]
+
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel("Count")
+    plt.title("Distribution of positive, negative and stop words")
+
+    plt.show()
